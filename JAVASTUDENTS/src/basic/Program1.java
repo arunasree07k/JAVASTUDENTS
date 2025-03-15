f106f90b2ee12148f581c308091c397fd4a3d197
arunasree07k <168274090+arunasree07k@users.noreply.github.com>
2025-03-15 19:47:28 +0530

    Update Program1.java

--- JAVASTUDENTS/src/basic/Program1.java
+++ JAVASTUDENTS/src/basic/Program1.java
@@ -4,7 +4,7 @@ public class Program1 {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.printl("java programming");
+		System.out.println("java programming");
 	}
 
 }
